962a8bfcbceed31361a6f64f264373b353e11858
Jacob M. <jacobmcewen7@gmail.com>
2026-03-31 21:27:16 -0600

    migration script added to add externalID column. FastAPI entry point setup.

---
+++ db/migrations/sqls/20260331165219-add-external-id-to-sanctioned-entities-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+ALTER TABLE sanctioned_entities
+DROP COLUMN external_id;
---
+++ db/migrations/sqls/20260331165219-add-external-id-to-sanctioned-entities-up.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+ALTER TABLE sanctioned_entities
+ADD COLUMN external_id VARCHAR(20);
