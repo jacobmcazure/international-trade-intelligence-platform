eea03cbfeedf069c696e8da1b2969c822ef712ee
Jacob M. <jacobmcewen7@gmail.com>
2026-07-07 21:29:12 -0600

    fixed sanctions route, started work on query

--- api/src/routes/sanctions.js
+++ api/src/routes/sanctions.js
@@ -5,15 +5,19 @@
  */
 async function sanctions (fastify, options) {
     fastify.get('/', async (request, reply) => {
+        const { country } = request.query
+
         const client = await fastify.pg.connect()
         try {
            const { rows } = await client.query(
-            'SELECT se.*, sa.address, sa.city_state_zip, sa.country, sa.remarks ' +
-            'FROM sanctioned_entities as se ' +
-            'INNER JOIN sanctions_addresses as sa on se.entity_id = sa.entity_id'
+            "SELECT DISTINCT ON (se.entity_id) se.*, sa.city_state_zip, sa.country, sa.remarks " +
+            "FROM sanctioned_entities as se " +
+            "INNER JOIN sanctions_addresses as sa on se.entity_id = sa.entity_id " +
+            "WHERE ($1::text IS NULL OR sa.country = $1) " + 
+            "ORDER BY se.entity_id DESC " + 
+            "LIMIT 1000", [country]
         )
-           const pong = await fastify.redis.ping() 
-           return {db: rows, redis: pong }
+           return {data: rows}
         } finally {
            client.release()
         }
