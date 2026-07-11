0142438c8ca1d7451137ab8c4db3bed17c7cffb1
Jacob M. <jacobmcewen7@gmail.com>
2026-07-10 22:44:09 -0600

    next js research, started on home page.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Geo-Intelligence Trade Platform</h1>
         <h3>International Relations Info, made easily accessible to you.</h3>
         <h4>Whether you're an economist, a student, or just curious, you can find
           what you're looking for here.
         </h4>
       </main>
+      <div>
+        <Link href="/explore" className="flex justify-center w-auto p-2 m-2 rounded-md shadow-md bg-sky-500 hover:bg-sky-300">Get Started</Link>
+      </div>
     </div>
   );
 }
