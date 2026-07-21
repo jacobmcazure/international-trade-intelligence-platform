bdb6e77b7e5cba3702e9aa320ecfbc5fb0b0ad78
Jacob M. <jacobmcewen7@gmail.com>
2026-07-20 21:50:39 -0600

    added pgadmin ui for db. working on tying search, map, and db.

--- frontend/app/explore/page.tsx
+++ frontend/app/explore/page.tsx
@@ -1,7 +1,17 @@
 import WorldMap from "../components/WorldMap";
 import SearchBar from "../components/SearchBar";
 
+import { useState, useEffect } from "react";
+
 export default function explore() {
+
+    // add onSearch prop to searchbar
+    // const [selectedCountry, sanctionsData, isDrawerOpen, loading] = useState([])
+
+    const handleSelect = (countryName : string) => {
+
+    }
+
     return (
         <div className="relative flex min-h-screen w-full bg-blue-300 flex-col items-center justify-center gap-4 px-6 py-24 text-center">
             <div className="max-w-2xl">
