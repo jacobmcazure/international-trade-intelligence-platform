a4033510ffa808ae7e6cf3ab7507001f46f01ac2
Jacob M. <jacobmcewen7@gmail.com>
2026-09-15 08:12:59 -0600

    removed unused commodities directory.

--- frontend/app/commodities/[hsCode]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function commoditiesHsCode({ params }: { params: { hsCode: string } }) {
-    const { hsCode } = await params
-    return (
-        <div>
-            commodities by hs code
-        </div>
-    )
-}
